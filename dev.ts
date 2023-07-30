7c1d9fb4492f2bc1fad96bd2be8515a407b45e4b
Joseph Siddall <j.b.siddall@gmail.com>
2023-07-30 18:59:15 +0100

    not sure

--- dev.ts
+++ dev.ts
@@ -8,18 +8,8 @@ const betterClient = createHubspotClient(client)
 
 const main = async () => {
     const results = await betterClient.contacts.findMany({select: {address: true, hs_createdate: true}})
+    betterClient.contacts.findMany({select: {address: true}})
     console.log('results', results[0].properties)
 }
-
-/*
-    properties: {
-      createdate: '2023-07-16T17:37:35.683Z',
-      email: 'bh@hubspot.com',
-      firstname: 'Brian',
-      hs_object_id: '51',
-      lastmodifieddate: '2023-07-16T17:37:54.419Z',
-      lastname: 'Halligan (Sample Contact)'
-    },
-*/
 
 main()
